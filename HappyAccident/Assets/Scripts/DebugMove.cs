fa72a6b4a30db6c8adca7c91951a3f26d6fe6dc1
ddiep003@ucr.edu <ddiep003@ucr.edu>
2018-01-27 03:41:24 -0800

    update

--- HappyAccident/Assets/Scripts/DebugMove.cs
+++ HappyAccident/Assets/Scripts/DebugMove.cs
@@ -27,7 +27,7 @@ public class DebugMove : MonoBehaviour {
     {
         float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
         Vector3 pos_move = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
-        transform.position = new Vector3(pos_move.x, transform.position.y, pos_move.z);
+        transform.position = new Vector3(pos_move.x, pos_move.y, pos_move.z);
 
     }
 }
