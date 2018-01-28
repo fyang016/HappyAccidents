f2859a84b9ebc6c06c6cbf9e7b505747a35dcfef
Timothy Touch <chaosbounder@gmail.com>
2018-01-28 08:38:41 -0800

    Add particle effects when energy collides with Simple Block

--- HappyAccident/Assets/Scripts/EnergyCollide.cs
+++ HappyAccident/Assets/Scripts/EnergyCollide.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnergyCollide : MonoBehaviour {
+	public GameObject deathEffect;
 
 	void OnCollisionEnter2D (Collision2D collideInfo) {
-		if (collideInfo.collider.tag == "Obstacle") {
-			Destroy (gameObject);
-		}
-		if (collideInfo.collider.tag == "Wave") {
+		if (collideInfo.collider.tag == "Obstacle" || collideInfo.collider.tag == "Wave") {
+			Instantiate (deathEffect, transform.position, Quaternion.identity);
 			Destroy (gameObject);
 		}
 	}
+
 }
