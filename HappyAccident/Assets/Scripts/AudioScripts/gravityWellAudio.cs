5750ec5aa26977a1c08e42f9346e2737d98c59a4
KingdomCross <alexchheng1998@yahoo.com>
2018-01-27 22:18:25 -0800

    Audio Update
    GravityWell audio on wave object with script
    simpleblock audio script

---
+++ HappyAccident/Assets/Scripts/AudioScripts/gravityWellAudio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    [RequireComponent(typeof(AudioSource))]
+public class gravityWellAudio : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+      
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collideInfo)
+    {
+        if (collideInfo.collider.tag == "Energy")
+        {
+            AudioSource gravityAudio = GetComponent<AudioSource>();
+            gravityAudio.Play();
+        }
+    }
+}
