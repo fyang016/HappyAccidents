c4972bd5c9ec53e6cfde5e4cbd00d95e63d9e492
KingdomCross <alexchheng1998@yahoo.com>
2018-01-28 13:15:57 -0800

    ButtonBox add
    buttonbox with sound effect
    fixed conductor art asset

---
+++ HappyAccident/Assets/ButtonAudio.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAudio : MonoBehaviour {
+
+    void OnMouseUp()
+    {
+        AudioSource buttonAudio = GetComponent<AudioSource>();
+        buttonAudio.Play();
+    }
+}
