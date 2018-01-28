60f55b9514090398727082f809f671e4db1fbf2c
coalvoltage <jim011@ucr.edu>
2018-01-28 10:00:49 -0800

    Credits has been completed

--- HappyAccident/Assets/Scripts/CreditStop.cs
+++ HappyAccident/Assets/Scripts/CreditStop.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CreditStop : MonoBehaviour {
+	public string creditTag;
+
+	[SerializeField] bool haveTriggered = false;
+
 	void OnTriggerEnter2D(Collider2D colInfo) {
-		colInfo.GetComponent<Rigidbody2D>().velocity = new Vector3 (0.0f, 0.0f, 0.0f);
+		if (!haveTriggered) {
+			if (colInfo.tag == creditTag) {
+				colInfo.attachedRigidbody.velocity = new Vector3 (0.0f, 0.0f, 0.0f);
+				haveTriggered = true;
+			}
+		}
 	}
 }
