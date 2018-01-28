a4ebb060bed680883e11e037d0e557ad76199e0e
KingdomCross <alexchheng1998@yahoo.com>
2018-01-27 17:02:18 -0800

    Teleporter Complete
    Need test with complete energy object, (Emitter Object)
    
    Teleporter Scripts folder with 2 scripts
    Teleporter Prefabs

---
+++ HappyAccident/Assets/Scripts/TeleporterScripts/teleportExitTouch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleportExitTouch : MonoBehaviour {
+
+
+    //Credit to EmitterEnergy script (Jim)
+    //void OnMouseDrag()
+    //{
+    //    float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+    //    Vector3 pos_move = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+    //    transform.position = new Vector3(pos_move.x, pos_move.y, pos_move.z);
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        transform.Rotate(0, 0, 90);
+    //    }
+
+    //}
+}
