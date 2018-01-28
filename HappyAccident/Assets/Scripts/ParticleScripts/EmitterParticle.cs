659460add14f0ff62c29314b2c2f1b26f7ea4c16
Sebastian Cornelio <cornelioericks@gmail.com>
2018-01-28 13:45:56 -0800

    apple

---
+++ HappyAccident/Assets/Scripts/ParticleScripts/EmitterParticle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmitterParticle : MonoBehaviour {
+	public GameObject emitterEffect;
+	public string userTag;
+
+	void OnTriggerEnter2D (Collider2D conInfo) {
+		if (conInfo.tag == userTag) {
+			Instantiate (emitterEffect, transform.position, Quaternion.identity);
+		}
+	}
+
+}
