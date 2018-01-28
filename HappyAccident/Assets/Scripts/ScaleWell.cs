7f4b3d2a2ed61a1f6594f929824d2ba65fd9235c
coalvoltage <jim011@ucr.edu>
2018-01-27 18:41:11 -0800

    Edited the Gravity Well a bit

--- HappyAccident/Assets/Scripts/ScaleWell.cs
+++ HappyAccident/Assets/Scripts/ScaleWell.cs
@@ -6,6 +6,7 @@ public class ScaleWell : MonoBehaviour {
 	Rigidbody2D m_Rigidbody;
 	Collider2D m_Collider;
 	public float scaleRadius = 2.0f;
+	public float angularVelo = 90.0f;
 	public bool isOn = true;
 
 	public void toggleWell() {
@@ -25,6 +26,7 @@ public class ScaleWell : MonoBehaviour {
 			m_Collider.enabled = false;
 		} else {
 			m_Rigidbody.transform.localScale = new Vector2 (scaleRadius,scaleRadius);
+			m_Rigidbody.angularVelocity = angularVelo;
 			m_Collider.enabled = true;
 		}
 	}
