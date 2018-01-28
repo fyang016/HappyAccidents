1040064c1b7a8bd4a354583bb7207bcad854e6d8
coalvoltage <jim011@ucr.edu>
2018-01-28 07:13:03 -0800

    prefab

---
+++ HappyAccident/Assets/Scripts/CreditStop.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditStop : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D colInfo) {
+		colInfo.GetComponent<Rigidbody2D>().velocity = new Vector3 (0.0f, 0.0f, 0.0f);
+	}
+}
