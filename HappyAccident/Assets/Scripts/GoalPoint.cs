48798427d0602ee9913295acacb236c2b66ad35a
coalvoltage <jim011@ucr.edu>
2018-01-27 21:10:45 -0800

    GoalPoint add functionality

--- HappyAccident/Assets/Scripts/GoalPoint.cs
+++ HappyAccident/Assets/Scripts/GoalPoint.cs
@@ -5,23 +5,18 @@ using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(AudioSource))]
 public class GoalPoint : MonoBehaviour {
-	private AssetBundle myLoadedAssetBundle;
-	private string[] scenePaths;
+	public string nextSceneName;
 	void Start() {
-		myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/AssetBundles/scenes");
-		scenePaths = myLoadedAssetBundle.GetAllScenePaths();
+		
 	}
 
 	void OnTriggerEnter2D(Collider2D colInfo) {
 		if (colInfo.tag == "Energy") {
 			AudioSource winSong = GetComponent<AudioSource> ();
 			winSong.Play ();
-			int nextSceneIndex = SceneManager.GetActiveScene ().buildIndex + 1;
-			Debug.Log ("Help");
-			if (SceneManager.sceneCount > nextSceneIndex) {
 
-				SceneManager.LoadSceneAsync (nextSceneIndex);
-			}
+			Debug.Log ("Help");;
+			SceneManager.LoadScene(nextSceneName);
 		}
 	}
 }
