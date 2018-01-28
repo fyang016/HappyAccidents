723a2140744880e9a7095986ae8a601f45baefcc
coalvoltage <jim011@ucr.edu>
2018-01-27 16:41:19 -0800

    Meta Files

--- HappyAccident/Assets/Scripts/DebugMove.cs
+++ HappyAccident/Assets/Scripts/DebugMove.cs
@@ -24,17 +24,13 @@ public class DebugMove : MonoBehaviour {
         float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
         Vector3 pos_move = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
         transform.position = new Vector3(pos_move.x, pos_move.y, pos_move.z);
-<<<<<<< HEAD
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
 			transform.Rotate(0, 0, 90);
 		}
-=======
-        transform.position = new Vector3(Mathf.Round(transform.position.x),
-                             Mathf.Round(transform.position.y),
-                             Mathf.Round(transform.position.z));
+		transform.position = new Vector3 (Mathf.Round (transform.position.x),
+			Mathf.Round (transform.position.y),
+			Mathf.Round (transform.position.z));
+	}
 
->>>>>>> refs/remotes/origin/master
-
-    }
 }
