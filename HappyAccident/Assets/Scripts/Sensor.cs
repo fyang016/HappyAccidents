49e5570d235169974a195e99414d275c81495f9a
ddiep003@ucr.edu <ddiep003@ucr.edu>
2018-01-28 02:15:31 -0800

    You cann't move parts when emitting now.

--- HappyAccident/Assets/Scripts/Sensor.cs
+++ HappyAccident/Assets/Scripts/Sensor.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Sensor : MonoBehaviour {
 
-	// Use this for initialization
+    // Use this for initialization
+    GameObject gamemanager;
 	void Start () {
-		
+        gamemanager = GameObject.Find("_GameManager");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        gamemanager.GetComponent<Manager>().components.Add(collision.gameObject);
+    }
 }
