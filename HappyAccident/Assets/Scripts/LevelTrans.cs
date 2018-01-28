6da1d2f48052bb59bf53df4b14d894c91adf373e
coalvoltage <jim011@ucr.edu>
2018-01-28 13:20:53 -0800

    Doing Trans & Credits

---
+++ HappyAccident/Assets/Scripts/LevelTrans.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelTrans : MonoBehaviour {
+	public float DelayTime = 10.0f;
+	public string nextSceneName;
+	bool goToNext = false;
+
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("delayTime");
+	}
+
+	IEnumerator delayTime() {
+		yield return new WaitForSeconds (DelayTime);
+		goToNext = true;
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			goToNext = true;
+		}
+		if (goToNext) {
+			SceneManager.LoadSceneAsync(nextSceneName);
+		}
+	}
+}
